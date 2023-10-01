1d69e789c88bdc9f8f9dd5631edb37fddb9c7be3
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-10-01 21:53:22 +0700

    create: constant.go

---
+++ constant/constant.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	const firstName string = "John"
+	fmt.Print("Hello ", firstName, "!\n")
+
+	const lastName = "Wick"
+	fmt.Print("Nice to meet you", lastName, "!\n")
+}
